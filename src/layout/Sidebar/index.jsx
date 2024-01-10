312659d03913d5c6b44f040ca69536fa239fe544
Damar Anshary <damar.galihanshary7@gmail.com>
2024-01-10 19:11:28 +0700

    add delete modal confirmation

--- src/layout/Sidebar/index.jsx
+++ src/layout/Sidebar/index.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <aside className="sticky left-0 top-0 flex min-h-screen w-1/12 flex-col flex-wrap gap-y-2 border border-transparent border-r-gray-200 bg-primary py-5">
-      <div className="flex flex-col items-center gap-y-10 pt-16 ">
+    <aside className="sticky left-0 top-0 flex h-screen w-1/12 flex-col flex-wrap gap-y-2 border border-transparent border-r-gray-200 bg-primary py-5">
+      <div className="sticky flex flex-col items-center gap-y-10 pt-16 ">
         <Link to="/" className="flex flex-col items-center">
           <AiOutlineHome className="inline-block text-white text-3xl xl:text-5xl" />
           <p className="text-center text-xs sm:text-sm md:text-base font-semibold text-white">Home</p>
