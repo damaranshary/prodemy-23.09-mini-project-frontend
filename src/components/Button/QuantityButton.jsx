f391206df502f1cd2a037fd7c9f9b539dc5b2f1b
Damar Anshary <damar.galihanshary7@gmail.com>
2024-01-11 09:55:27 +0700

    merging and adjusting from avis-edited-2

---
+++ src/components/Button/QuantityButton.jsx
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../../store/slices/productSlice";
+
+const QuantityButton = ({ productId, quantity }) => {
+  const dispatch = useDispatch();
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      setQty(event.target.value);
+    }
+  };
+
+  const handleClick = (operation) => {
+    let qty = quantity;
+    if (operation == "+") {
+      qty += 1;
+    } else if (operation == "-") {
+      if (qty == 0) {
+        qty = 0;
+      } else {
+        qty -= 1;
+      }
+    }
+    const payload = {
+      qty: qty,
+      id: productId,
+    };
+    dispatch(addToCart(payload));
+  };
+
+  return (
+    <>
+      <div className="flex w-fit items-center gap-1">
+        <button onClick={() => handleClick("-")}>-</button>
+        <input
+          className="w-10 rounded-lg border-2 border-slate-200 text-center text-sm"
+          type="text"
+          onKeyDown={handleKeyDown}
+          value={quantity}
+          onChange={(event) => event.target.value}
+        />
+        <button onClick={() => handleClick("+")}>+</button>
+      </div>
+    </>
+  );
+};
+
+export default QuantityButton;
--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -1,38 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    products: [],
-    cart: [],
-    wishlistConfirmed : false,
-    total: 0,
-};
+  products: [],
+  cart: [],
+  wishlistConfirmed: false,
+}
 
-const productSlice = createSlice ({
-    name:'shop',
-    initialState,
-    reducers:{
-        setProducts: (state, action) => {
-            state.products = action.payload;
-        },
-        addToCart: (state,action) => {
-            state.cart.push(action.payload);
-            state.total += Number (action.payload.price);
-        },
-        removeFromCart: (state, action) => {
-            const item = state.cart.find(item => item.id === action.payload);
-            if (item) {
-                state.total -= Number (item.price);
-                state.cart = state.cart.filter(item => item.id !== action.payload);
-            }
-        },
-        
-        confirmWishlist: (state) => {
-            state.wishlistConfirmed = true;
-            state.total = state.cart.reduce((total, item) => total + Number (item.price), 0);
-        }
-    }
-});
-export const { setProducts, addToCart, removeFromCart, confirmWishlist } = productSlice.actions;
-export default productSlice.reducer;
+const productSlice = createSlice({
+  name: "shop",
+  initialState,
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload
+    },
+    addToCart: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload.id)
+      if (item) {
+        item.qty = action.payload.qty
+      } else {
+        state.cart.push(action.payload)
+      }
+      console.log(state.cart)
+    },
+    removeFromCart: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload)
+      if (item) {
+        state.cart = state.cart.filter((item) => item.id !== action.payload)
+      }
+    },
 
+    reserCart: (state) => {
+      state.cart = []
+    },
 
+    confirmWishlist: (state) => {
+      state.wishlistConfirmed = true
+      state.total = state.cart.reduce((total, item) => total + Number(item.price), 0)
+    },
+  },
+})
+export const { setProducts, addToCart, removeFromCart, confirmWishlist, reserCart } = productSlice.actions
+export default productSlice.reducer
