58db6d5f9bcad7f20417d5e05ed3becdc78dde43
Damar Anshary <damar.galihanshary7@gmail.com>
2024-01-13 12:53:42 +0700

    fixing cart (daftar pesanan) stying

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -14,6 +14,7 @@ import QuantityButton from "../../components/Button/QuantityButton";
 import { BeatLoader } from "react-spinners";
 import { AiFillPlusCircle, AiOutlinePlusCircle } from "react-icons/ai";
 import { useSearchParams } from "react-router-dom";
+import { RadioGroup } from "@headlessui/react";
 
 function Home() {
   const dispatch = useDispatch();
@@ -33,6 +34,14 @@ function Home() {
   } = getAllProducts(category, queryParams ? queryParams : null, sort);
 
   const { data: categoriesData } = getAllCategories();
+
+  const [paymentMethod, setPaymentMethod] = useState("");
+  const paymentMethodOptions = [
+    { name: "Gopay", value: "gopay", logo: "/assets/gopay-logo.png" },
+    { name: "LinkAja", value: "link-aja", logo: "/assets/linkaja-logo.png" },
+    { name: "DANA", value: "dana", logo: "/assets/dana-logo.png" },
+    { name: "BCA", value: "bca-bank", logo: "/assets/bca-logo.webp" },
+  ];
 
   const handleSortChange = (event) => {
     setSort(event.target.value);
@@ -89,7 +98,7 @@ function Home() {
             onClick={() => {
               setCategory(null);
             }}
-            className={`flex flex-row items-center justify-between gap-x-2 rounded-full border border-gray-300 px-5 py-2 hover:cursor-pointer font-semibold ${
+            className={`flex flex-row items-center justify-between gap-x-2 rounded-full border border-gray-300 px-5 py-2 font-semibold hover:cursor-pointer ${
               category === null
                 ? "bg-primary p-3 text-sm text-white"
                 : "bg-white text-primary"
@@ -103,7 +112,7 @@ function Home() {
               onClick={() => {
                 setCategory(categoryData.id);
               }}
-              className={`flex flex-row items-center justify-between gap-x-2 rounded-full border border-gray-300 px-5 py-2 hover:cursor-pointer font-semibold ${
+              className={`flex flex-row items-center justify-between gap-x-2 rounded-full border border-gray-300 px-5 py-2 font-semibold hover:cursor-pointer ${
                 categoryData.id === category
                   ? "bg-primary text-white"
                   : "bg-white"
@@ -128,7 +137,7 @@ function Home() {
             </form>
             <select
               onChange={handleSortChange}
-              className="ms-0 rounded-full border border-gray-300 px-5"
+              className="ms-0 rounded-full border border-gray-300 px-5 pe-10"
             >
               <option value="nameAsc">Nama A-Z</option>
               <option value="nameDesc">Nama Z-A</option>
@@ -178,71 +187,90 @@ function Home() {
         </div>
       </div>
       <div className="border-xl sticky right-0 top-0 flex h-fit flex-1 flex-col gap-1 rounded-2xl border border-gray-300">
-        <div className="flex w-full flex-col gap-5 p-5 ">
+        <div className="flex w-full flex-col gap-y-5 p-5">
           <h1 className="text-lg font-semibold">Daftar Pesanan</h1>
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-5">
-              {cart?.map((list) => (
-                <div className="flex justify-between" key={list.id}>
-                  <div className="flex gap-3">
-                    <img
-                      src={list.image}
-                      alt="Gambar Produk"
-                      className="w-16 rounded-lg"
-                    />
-                    <div>
-                      <h1 className="text-sm font-semibold ">{list.title}</h1>
-                      <h1 className="text-sm">
-                        Rp. {list.price.toLocaleString("ID-id")}
-                      </h1>
+          {cart.length > 0 ? (
+            <>
+              {" "}
+              <div className="flex flex-col gap-4 ">
+                {cart?.map((list) => (
+                  <div
+                    className="flex items-center justify-between border border-transparent border-b-gray-200 pb-3"
+                    key={list.id}
+                  >
+                    <div className="flex items-center gap-3">
+                      <img
+                        src={list.image}
+                        alt="Gambar Produk"
+                        className="h-16 w-16 rounded-lg"
+                      />
+                      <div className="flex flex-col">
+                        <h4 className="text-sm font-semibold ">{list.title}</h4>
+                        <p className="text-sm">
+                          Rp. {list.price.toLocaleString("ID-id")}
+                        </p>
+                      </div>
                     </div>
+                    <QuantityButton productId={list.id} quantity={list.qty} />
                   </div>
-                  <QuantityButton productId={list.id} quantity={list.qty} />
+                ))}
+              </div>
+              <div className="flex flex-col gap-y-5">
+                <div className="flex items-center justify-between border-y-2 border-transparent border-y-gray-300 py-3">
+                  <h1 className="text-base">Total Harga</h1>
+                  <h1 className="text-lg font-semibold">
+                    Rp.{" "}
+                    {cart
+                      .reduce(
+                        (accumulator, product) =>
+                          accumulator + product.price * product.qty,
+                        0,
+                      )
+                      .toLocaleString("ID-id")}
+                  </h1>
                 </div>
-              ))}
-            </div>
-          </div>
-          <div className="flex justify-between">
-            <h1 className="tex-sm font-semibold">Total</h1>
-            <h1 className="tex-sm font-semibold">
-              Rp.{" "}
-              {cart.reduce(
-                (accumulator, product) =>
-                  accumulator + product.price * product.qty,
-                0,
-              )}
-            </h1>
-          </div>
-          <h1 className="text-lg font-semibold">Payment Method</h1>
-          <div className="flex justify-around">
-            <button>
-              <img
-                src="https://pbs.twimg.com/profile_images/966253495547342848/_bjmYta5_400x400.jpg"
-                alt="Dana"
-                className="w-16 rounded-2xl"
-              />
-            </button>
-            <button>
-              <img
-                src="https://seeklogo.com/images/B/bank-transfer-logo-291DE7CDB2-seeklogo.com.png"
-                alt="Transfer Bank"
-                className="h-16 w-16 rounded-2xl"
-              />
-            </button>
-            <button>
-              <img
-                src="https://fintech.id/storage/files/shares/logo/logofi2/gopay-1.jpg"
-                alt="Gopay"
-                className="h-16 w-16 rounded-2xl"
-              />
-            </button>
-          </div>
-          <button
-            onClick={handleConfirmWishlist}
-            className="rounded-xl bg-primary p-2 text-white"
-          >
-            Confirm Buy
-          </button>
+                <RadioGroup value={paymentMethod} onChange={setPaymentMethod}>
+                  <RadioGroup.Label className="text-base font-semibold">
+                    Metode Pembayaran
+                  </RadioGroup.Label>
+                  {paymentMethodOptions.map((option) => (
+                    <RadioGroup.Option
+                      key={option.name}
+                      as={Fragment}
+                      value={option.value}
+                    >
+                      {({ checked }) => (
+                        <div
+                          className={`mt-2 flex flex-row items-center justify-between rounded-xl border border-gray-200 px-5 py-2 hover:cursor-pointer ${
+                            checked ? "bg-slate-500 text-white" : ""
+                          }`}
+                        >
+                          <div className="flex w-full flex-row justify-between">
+                            <p className="text-sm font-semibold">
+                              {option.name}
+                            </p>
+                            <img
+                              src={option.logo}
+                              alt={option.name}
+                              className="ml-2 h-5"
+                            />
+                          </div>
+                        </div>
+                      )}
+                    </RadioGroup.Option>
+                  ))}
+                </RadioGroup>
+              </div>
+              <button
+                onClick={handleConfirmWishlist}
+                className="rounded-xl bg-primary p-2 text-white font-semibold"
+              >
+                Konfirmasi Pesanan
+              </button>
+            </>
+          ) : (
+            <p className="text-center">Keranjang Kosong</p>
+          )}
         </div>
       </div>
     </div>
